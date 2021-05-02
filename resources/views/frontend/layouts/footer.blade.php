f5fa233f8c13a8f60f7f6461f6fcf20b42ff50aa
Harun-or-rashid <harun35-2093@diu.edu.bd>
2021-05-02 16:55:47 +0600

    footer added with extra foot

---
+++ resources/views/frontend/layouts/footer.blade.php
@@ -0,0 +1,80 @@
+<div class="row">
+    <div class="col-md-4">
+        <div class="logo same-mb">
+            <a href="">
+                <img style="" src="{{asset('assets/front-end')}}/images/logo-khan.png" alt="">
+            </a>
+        </div>
+        <div class="container same-mb question d-flex   ">
+            <div class="icon col-md-1" style=" font-size: 35px;">
+                <span class="icon fa fa-question-circle"></span>
+            </div>
+            <div class="contact " style="margin:0 25px">
+                <h4>Got Questions ?</h4>
+                <span class="header-text-1">(+880) 1937 990 932</span><br>
+                <span class="header-text-2">(+880) 1937 990 933</span>
+            </div>
+        </div>
+        <div class="container sign-up same-mb">
+            <h4>Sign Up For Special Offer</h4>
+            <form class="form-inline my-2 my-lg-0">
+                <input class="form-control mr-sm-2" type="search" placeholder="Your email here" aria-label="Search">
+                <button class="btn btn-outline-warning my-2 my-sm-0" type="submit">Sign-Up</button>
+            </form>
+        </div>
+
+    </div>
+    <div class="col-md-2">
+        <h4 class="" style="text-align: center">Useful Links</h4>
+        <ul class="useful-links">
+            <li class=""><a class="btn btn-transparent text-warning" href="">Our History</a></li>
+            <li><a class="btn btn-transparent text-warning" href="">Resturant Menu</a></li>
+            <li><a class="btn btn-transparent text-warning" href="">Local Branches</a></li>
+{{--            <li><a class="btn btn-transparent text-warning" href="">Franchise</a></li>--}}
+            <li><a class="btn btn-transparent text-warning" href="">Our Gallery</a></li>
+            <li><a class="btn btn-transparent text-warning" href="">Contact Us</a></li>
+            <li><a class="btn btn-transparent text-warning" href="">About</a></li>
+        </ul>
+    </div>
+    <div class="col-md-2">
+        <div class=" same-mb">
+            <a href="">
+                <img style="" class="footer-img" src="https://khanemajlis.com/public/assets/front-end/images/ll.jpeg" alt="">
+            </a>
+        </div>
+    </div>
+    <div class="col-md-4">
+        <h4 class="same-mb" style="text-align: center">Corporate Office</h4>
+        <div class="location d-flex">
+            <div class="icon col-md-1" style=" font-size: 18px;">
+                <span class="icon fa fa-home"></span>
+            </div>
+            <div class="content">
+<p>HOUSE-47,RD NO:13,BLOCK: E,BANANI 1213,DHAKA</p>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="foot same-mb">
+    <div class="row d-flex">
+        <div class="col-md-6">
+            <h5>Copyright © Custom Digital. All rights reserved.</h5>
+        </div>
+        <div class="col-md-6 payment-img">
+            <a href="">
+                <img style="" src="{{asset('assets/front-end')}}/images/payment/bkash.png" alt="">
+            </a>
+            <a href="">
+                <img style="" src="{{asset('assets/front-end')}}/images/payment/Master.png" alt="">
+            </a>
+            <a href="">
+                <img style="" src="{{asset('assets/front-end')}}/images/payment/Nagad.png" alt="">
+            </a>
+            <a href="">
+                <img style="" src="{{asset('assets/front-end')}}/images/payment/Visa.png" alt="">
+            </a>
+
+        </div>
+    </div>
+</div>
+
